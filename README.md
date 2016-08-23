8f62d3d688975401491d09f06b66e81be6dd87ef
treylitefm <griffin.omar@gmail.com>
2016-08-22 22:55:10 -0700

    Bottom half of triangle rendering photo correctly
    Signed-off-by: treylitefm <griffin.omar@gmail.com>

---
+++ README.md
@@ -0,0 +1,1 @@
+Slowly adding all source to my totally awesome portfolio site.
--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
     $('.row').hover(function() {
-        $(this).children('.inner').addClass('hover');
-        $(this).children('.content').addClass('hover');
+        $(this).children('.bottom').addClass('hover');
+        $(this).children('.top').addClass('hover');
     }, function() {
-        $(this).children('.inner').removeClass('hover');
-        $(this).children('.content').removeClass('hover');
+        $(this).children('.bottom').removeClass('hover');
+        $(this).children('.top').removeClass('hover');
     });
 });
