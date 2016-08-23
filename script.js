641609131b8fb08cb2b574d070ffd761c6bcde00
treylitefm <griffin.omar@gmail.com>
2016-08-22 21:25:22 -0700

    Slide transition looking smooth finally
    Signed-off-by: treylitefm <griffin.omar@gmail.com>

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
     $('.row').hover(function() {
-        console.log('omarrr');
         $(this).children('.inner').addClass('hover');
+        $(this).children('.content').addClass('hover');
     }, function() {
         $(this).children('.inner').removeClass('hover');
+        $(this).children('.content').removeClass('hover');
     });
 });
